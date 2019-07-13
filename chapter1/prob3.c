a2e32653da3cffa51e95e468117a08ca36d6a295
David Keenan <willkeenan12@gmail.com>
2019-07-13 17:27:51 -0400

    Init commit

---
+++ chapter1/prob3.c
@@ -0,0 +1,26 @@
+/* Modify the temperature conversion program to print a heading
+above the table */
+
+#include <stdio.h>
+
+int main(void)
+{
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    printf("Fahrenheit | Celsius\n");
+    printf("---------------------\n");
+
+    fahr = lower;
+    while (fahr <= upper) {
+        celsius = (5.0 / 9.0) * (fahr - 32.0);
+        printf("%10.0f | %6.1f\n", fahr, celsius);
+        fahr += step;
+    }
+
+    return 0;
+}
---
+++ chapter1/prob5.c
@@ -0,0 +1,26 @@
+/* Modify the temperature conversion program to print the table in
+reverse order, that is, from 300 degrees to O */
+
+#include <stdio.h>
+
+int main(void)
+{
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    printf("Fahrenheit | Celsius\n");
+    printf("---------------------\n");
+
+    fahr = upper;
+    while (fahr >= lower) {
+        celsius = (5.0 / 9.0) * (fahr - 32.0);
+        printf("%10.0f | %6.1f\n", fahr, celsius);
+        fahr -= step;
+    }
+
+    return 0;
+}
