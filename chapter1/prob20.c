b40bb81fbf14aa1618d1688c015a02b3e01023e0
David Keenan <willkeenan12@gmail.com>
2020-03-27 22:59:15 -0400

    Solution to problem 20

---
+++ chapter1/prob20.c
@@ -0,0 +1,33 @@
+/* Write a program detab that replaces tabs in the input with the proper number
+of blanks to space to the next tab stop. */
+
+/* Question: Assume a fixed set of tab stops, say every n columns. Should n be a variable
+ *   or a symbolic parameter?
+ * Answer: It should probably be a variable so it can be changed at runtime
+ *   without re-compiling (eg through a command line argument)  */
+
+#include <stdio.h>
+
+#define TABSIZE 8
+
+int main()
+{
+    int c = EOF, pos = 0;
+
+    while ((c = getchar()) != EOF) {
+        if (c == '\t') {
+            for (int i = 0; i < (TABSIZE - (pos % TABSIZE)); i++) {
+                printf(" ");
+            }
+        } else {
+            printf("%c", c);
+            pos++;
+        }
+
+        if (c == '\n' || c == '\t') {
+            pos = 0;
+        }
+    }
+
+    return 0;
+}
