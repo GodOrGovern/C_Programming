a68fb820c28b5494d4cb61c86370b939598954f7
David Keenan <willkeenan12@gmail.com>
2020-04-03 01:17:46 -0400

    Start entab program

---
+++ chapter1/prob21.c
@@ -0,0 +1,34 @@
+/* Write a program entab that replaces strings of blanks by the minimum number
+ * of tabs and blanks to achieve the same spacing. Use the same tab stops as
+ * for detab. */
+
+/* Question: When either a tab or a single blank would suffice to reach a tab
+ * stop, which should be given preference?
+ * Answer: A blank should be given preference as tabs do not have a uniform
+ * width. */
+
+#include <stdio.h>
+
+#define TABSIZE 8
+
+int main()
+{
+    int c = EOF, spaces = 0, pos = 0;
+
+    while ((c = getchar()) != EOF) {
+        if (c == ' ') {
+            spaces += 1;
+        } else if (c == '\n' || c == '\t') {
+            spaces = pos = 0;
+            putchar(c);
+        } else {
+            if (spaces > 0) {
+
+            }
+        }
+
+        prev = c;
+    }
+
+    return 0;
+}
