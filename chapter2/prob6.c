dbf8498a799bf294edd345b35778d08d3a29e1f2
David Keenan <willkeenan12@gmail.com>
2020-07-16 17:12:58 -0400

    Replace int with unsigned int where appropriate

--- chapter2/prob6.c
+++ chapter2/prob6.c
@@ -11,10 +11,10 @@
  *   Replace the 'n' bits that begin at position 'p' in 'x' with the right-most
  *   'n' bits of 'y'
  * Parameters:
- *   x (int): all bits except for the 'n' bits beginning at position 'p'
+ *   x (uint): all bits except for the 'n' bits beginning at position 'p'
  *   p (int): position where bits in 'x' will be substituted for bits in 'y'
  *   n (int): number of bits in 'x' to replace with bits from 'y'
- *   y (int): 'n' right-most bits are substituted into 'x'
+ *   y (uint): 'n' right-most bits are substituted into 'x'
  * Return Value:
  *   'x' with the 'n' bits at position 'p' substituted with the right-most 'n'
  *   bits of 'y'
@@ -24,7 +24,7 @@
  *   position). Finally, the trailing bits that remain from 'x' are added. The
  *   final sum is returned
 */
-int setbits(int x, int p, int n, int y)
+unsigned int setbits(unsigned int x, int p, int n, unsigned int y)
 {
     return (x & ~((1 << (p+1)) - 1)) + ((y & (1 << n) - 1) << (p-n+1)) + (x & (1 << (p-n)) - 1);
 }
--- chapter2/prob7.c
+++ chapter2/prob7.c
@@ -10,7 +10,7 @@
  * Summary:
  *   Return 'x' with the 'n' bits starting at position 'p' inverted
  * Parameters:
- *   x (int): all bits except for the 'n' bits beginning at position 'p'
+ *   x (uint): all bits except for the 'n' bits beginning at position 'p'
  *   p (int): position where bits in 'x' will be inverted
  *   n (int): number of bits in 'x' to invert
  * Return Value:
@@ -19,7 +19,7 @@
  *   'x' is XORed with a bit string that is 0 everywhere except for the 'n'
  *   bits at position 'p'. The result is returned
 */
-int invert(int x, int p, int n)
+unsigned int invert(unsigned int x, int p, int n)
 {
     return x ^ (((1 << n) - 1) << (p-n+1));
 }
